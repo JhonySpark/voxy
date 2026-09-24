4bf1dc4795cc17522475379d21a3fc99422b9fde
Jhonatan Ciriaco <jhonatanferreira13@hotmail.com>
2026-09-24 16:40:03 -0300

    chore: add electron main process output and ignore build directory

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,45 +1,33 @@
-import { BrowserWindow, app, desktopCapturer, ipcMain } from "electron";
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
+import { BrowserWindow as e, app as t, desktopCapturer as n, ipcMain as r } from "electron";
+import { dirname as i, join as a } from "node:path";
+import { fileURLToPath as o } from "node:url";
 //#region electron/main.ts
-var __dirname = dirname(fileURLToPath(import.meta.url));
-if (process.release.name === "win32") app.disableHardwareAcceleration();
-var win = null;
-async function createWindow() {
-	win = new BrowserWindow({
+var s = i(o(import.meta.url));
+process.release.name === "win32" && t.disableHardwareAcceleration();
+var c = null;
+async function l() {
+	c = new e({
 		title: "Voxy",
 		width: 1024,
 		height: 768,
-		autoHideMenuBar: true,
+		autoHideMenuBar: !0,
 		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false
+			nodeIntegration: !0,
+			contextIsolation: !1
 		}
-	});
-	win.removeMenu();
-	if (process.env.VITE_DEV_SERVER_URL) {
-		win.loadURL(process.env.VITE_DEV_SERVER_URL);
-		win.webContents.openDevTools();
-	} else win.loadFile(join(__dirname, "../dist/index.html"));
+	}), c.removeMenu(), process.env.VITE_DEV_SERVER_URL ? (c.loadURL(process.env.VITE_DEV_SERVER_URL), c.webContents.openDevTools()) : c.loadFile(a(s, "../dist/index.html"));
 }
-app.whenReady().then(() => {
-	ipcMain.handle("DESKTOP_CAPTURER_GET_SOURCES", async (event, opts) => {
-		return (await desktopCapturer.getSources(opts)).map((source) => ({
-			id: source.id,
-			name: source.name,
-			thumbnail: source.thumbnail.toDataURL()
-		}));
-	});
-	createWindow();
-});
-app.on("window-all-closed", () => {
-	win = null;
-	if (process.platform !== "darwin") app.quit();
-});
-app.on("activate", () => {
-	const allWindows = BrowserWindow.getAllWindows();
-	if (allWindows.length) allWindows[0].focus();
-	else createWindow();
+t.whenReady().then(() => {
+	r.handle("DESKTOP_CAPTURER_GET_SOURCES", async (e, t) => (await n.getSources(t)).map((e) => ({
+		id: e.id,
+		name: e.name,
+		thumbnail: e.thumbnail.toDataURL()
+	}))), l();
+}), t.on("window-all-closed", () => {
+	c = null, process.platform !== "darwin" && t.quit();
+}), t.on("activate", () => {
+	let t = e.getAllWindows();
+	t.length ? t[0].focus() : l();
 });
 //#endregion
 export {};
